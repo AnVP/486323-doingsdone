d86e18b4c28a28e0c3add68edc3d1ce85104bc69
AnVP <amltrp@mail.ru>
2018-11-27 01:06:39 +0500

    Ошибка

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE 486323-doingsdone
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+USE 486323-doingsdone;
+
+CREATE TABLE projects (
+  project_id INT AUTO_INCREMENT PRIMARY KEY,
+  project_name VARCHAR(128) NOT NULL UNIQUE,
+  user_id INT,
+
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE tasks (
+  task_id INT AUTO_INCREMENT PRIMARY KEY,
+  creation_date DATETIME,
+  execution_date DATETIME,
+  task_status INT DEFAULT 0,
+  task_name VARCHAR(128) NOT NULL,
+  task_file VARCHAR(128),
+  task_deadline DATETIME,
+  user_id INT,
+  project_id INT,
+
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (project_id) REFERENCES projects(project_id)
+);
+
+CREATE TABLE users (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  date_registration DATETIME,
+  user_email VARCHAR(128) NOT NULL UNIQUE,
+  user_name VARCHAR(128) NOT NULL UNIQUE,
+  user_password VARCHAR(64) NOT NULL
+);
