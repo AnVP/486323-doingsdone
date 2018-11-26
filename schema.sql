b72a504b5be61f86bd9d8edbf37280b012fb7be8
AnVP <amltrp@mail.ru>
2018-11-27 01:34:27 +0500

    Исправляет схему БД

--- schema.sql
+++ schema.sql
@@ -1,10 +1,18 @@
-CREATE DATABASE 486323-doingsdone
+CREATE DATABASE IF NOT EXISTS doingsdone_486323
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-USE 486323-doingsdone;
+USE doingsdone_486323;
 
-CREATE TABLE projects (
+CREATE TABLE IF NOT EXISTS users (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  date_registration DATETIME,
+  user_email VARCHAR(128) NOT NULL UNIQUE,
+  user_name VARCHAR(128) NOT NULL UNIQUE,
+  user_password VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projects (
   project_id INT AUTO_INCREMENT PRIMARY KEY,
   project_name VARCHAR(128) NOT NULL UNIQUE,
   user_id INT,
@@ -12,7 +20,7 @@ CREATE TABLE projects (
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE tasks (
+CREATE TABLE IF NOT EXISTS tasks (
   task_id INT AUTO_INCREMENT PRIMARY KEY,
   creation_date DATETIME,
   execution_date DATETIME,
@@ -25,12 +33,4 @@ CREATE TABLE tasks (
 
   FOREIGN KEY (user_id) REFERENCES users(user_id),
   FOREIGN KEY (project_id) REFERENCES projects(project_id)
-);
-
-CREATE TABLE users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  date_registration DATETIME,
-  user_email VARCHAR(128) NOT NULL UNIQUE,
-  user_name VARCHAR(128) NOT NULL UNIQUE,
-  user_password VARCHAR(64) NOT NULL
 );
