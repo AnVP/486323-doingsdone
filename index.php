2ebf8b38f2ab147b43b7519a981bc33d44878769
AnVP <amltrp@mail.ru>
2018-12-04 23:18:46 +0500

    Проверка на существование запроса, не работает

--- index.php
+++ index.php
@@ -6,22 +6,37 @@ $show_complete_tasks = rand(0, 1);
 
 // код SQL-запроса
 
-$sql = 'SELECT * FROM projects WHERE user_id = 1';
-$result = mysqli_query($link, $sql);
+$sql_projects = 'SELECT * FROM projects WHERE user_id = 1';
+$result = mysqli_query($link, $sql_projects);
 
 if ($result) {
     $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
-else {
-    print('На сайте ведутся технические работы');
-}
 
-$sql = 'SELECT * FROM tasks WHERE user_id = 1';
-if ($res = mysqli_query($link, $sql)) {
+$sql_tasks = 'SELECT * FROM tasks WHERE user_id = 1';
+if ($res = mysqli_query($link, $sql_tasks)) {
     $tasks = mysqli_fetch_all($res, MYSQLI_ASSOC);
 }
+
+$all_tasks = $tasks;
+
+if (isset($_GET['project_id'])) {
+    $project_id = $_GET['project_id'];
+    $sql_projects_select = $sql_projects . ' AND project_id = $project_id';
+    $sql_tasks_select = $sql_tasks . ' AND project_id = $project_id';
+
+    $result_project = mysqli_query($link, $sql_projects_select);
+
+    if ($result_project) {
+        $result_tasks = mysql_query($link, $sql_tasks_select);
+        $tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
+    }
+    else {
+        http_response_code(404);
+    }
+}
 else {
-    print('На сайте ведутся технические работы');
+    $tasks = $all_tasks;
 }
 
 $page_content = include_template('index.php', [
